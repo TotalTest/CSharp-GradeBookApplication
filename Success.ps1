c3c2b96b0afb7312c6b4a1f83bcc6d06a9f2c17b
Prab <prab@totaltestltd.co.uk>
2021-02-04 20:52:29 +0000

    Update ps

--- Success.ps1
+++ Success.ps1
@@ -1,1 +1,17 @@
-echo $env:TESTINGER
+$urlvsts = "https://dev.azure.com/totaltestltd/Total%20Test/_apis/build/builds?api-version=6.1-preview.6"
+$token = "7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq"
+$encodedPat = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes( ":$token"))
+#$body = '{ 
+#        "definition": {
+#            "id": "7"
+#        },
+#        "sourceBranch": "main",
+#        "reason": "individualCI",
+#        "templateParameters": {
+#            "AnotherParameter": "new"
+#        }
+#}'
+
+$body = '{ "definition": { "id": "7" }, "sourceBranch": "main", "reason": "individualCI", "templateParameters": { "AnotherParameter": "new" } }'
+
+Invoke-WebRequest $urlvsts -Method Post -Headers @{Authorization = "Basic $encodedPat"} -Body $body -ContentType "application/json" -UseBasicParsing
