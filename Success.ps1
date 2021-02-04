e31af9ec7d04c3c8e7ebbdd4702f96aab008cf8a
Prab <prab@totaltestltd.co.uk>
2021-02-04 22:03:01 +0000

    test

--- Success.ps1
+++ Success.ps1
@@ -1,17 +1,22 @@
-$urlvsts = "https://dev.azure.com/totaltestltd/Total%20Test/_apis/build/builds?api-version=6.1-preview.6"
-$token = "7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq"
-$encodedPat = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes( ":$token"))
-#$body = '{ 
-#        "definition": {
-#            "id": "7"
-#        },
-#        "sourceBranch": "main",
-#        "reason": "individualCI",
-#        "templateParameters": {
-#            "AnotherParameter": "new"
-#        }
-#}'
+$MyPat = '7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq'
+$B64Pat = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":$MyPat"))
+git -c http.extraHeader="Authorization: Basic $B64Pat" branch https://totaltestltd@dev.azure.com/totaltestltd/Total%20Test/_git/AtataPoc
 
-$body = '{ "definition": { "id": "7" }, "sourceBranch": "main", "reason": "individualCI", "templateParameters": { "AnotherParameter": "new" } }'
 
-Invoke-WebRequest $urlvsts -Method Post -Headers @{Authorization = "Basic $encodedPat"} -Body $body -ContentType "application/json" -UseBasicParsing
+#$urlvsts = "https://dev.azure.com/totaltestltd/Total%20Test/_apis/build/builds?api-version=6.1-preview.6"
+#$token = "7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq"
+#$encodedPat = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes( ":$token"))
+##$body = '{ 
+##        "definition": {
+##            "id": "7"
+##        },
+##        "sourceBranch": "main",
+##        "reason": "individualCI",
+##        "templateParameters": {
+##            "AnotherParameter": "new"
+##        }
+##}'
+#
+#$body = '{ "definition": { "id": "7" }, "sourceBranch": "main", "reason": "individualCI", "templateParameters": { "AnotherParameter": "new" } }'
+#
+#Invoke-WebRequest $urlvsts -Method Post -Headers @{Authorization = "Basic $encodedPat"} -Body $body -ContentType "application/json" -UseBasicParsing
