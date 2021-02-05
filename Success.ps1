1a40fcd5b355e5f6c043daf3ed95413d6690d6d2
Prab <prab@totaltestltd.co.uk>
2021-02-05 15:01:07 +0000

    test

--- Success.ps1
+++ Success.ps1
@@ -1,7 +1,7 @@
 $MyPat = '7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq'
 $B64Pat = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":$MyPat"))
-git -c http.extraHeader="Authorization: Basic $B64Pat" branch https://totaltestltd@dev.azure.com/totaltestltd/Total%20Test/_git/AtataPoc
-
+git -c http.extraHeader="Authorization: Basic $B64Pat" clone https://totaltestltd@dev.azure.com/totaltestltd/Total%20Test/_git/AtataPoc
+git branch
 
 #$urlvsts = "https://dev.azure.com/totaltestltd/Total%20Test/_apis/build/builds?api-version=6.1-preview.6"
 #$token = "7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq"
