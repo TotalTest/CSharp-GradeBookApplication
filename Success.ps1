1aeac5c5a70a26f7186cf986a030df50e3e63c5f
Prab <prab@totaltestltd.co.uk>
2021-02-06 20:18:27 +0000

    test

--- Success.ps1
+++ Success.ps1
@@ -2,17 +2,15 @@
 $env:GIT_REDIRECT_STDERR = '2>&1'
 git config --global user.email "prab@totaltestltd.co.uk"
 git config --global user.name "Prab"
-#git clone https://prab:$($env:git_creds)@totaltestltd.visualstudio.com/SampleProject/_git/SampleProject C:\projects\atata
-git clone https://totaltestltd@dev.azure.com/totaltestltd/SampleProject/_git/SampleProject C:\projects\atata
+git clone https://prab:$($env:git_creds)@totaltestltd.visualstudio.com/SampleProject/_git/SampleProject C:\projects\atata
 git remote remove origin
-git remote add origin https://prab:$($env:git_creds)@totaltestltd.visualstudio.com/SampleProject/_git/SampleProject
+#git remote add origin https://prab:$($env:git_creds)@totaltestltd.visualstudio.com/SampleProject/_git/SampleProject
+git remote add origin https://totaltestltd@dev.azure.com/totaltestltd/SampleProject/_git/SampleProject
 cd C:\projects\atata
 git branch
-git checkout -b testh
-git push -u origin testh
+git checkout -b testi
+git push -u origin testi
 #git push --set-upstream origin testg
-
-echo $LASTEXITCODE
 
 if ($LASTEXITCODE -ne 0)  { 
        Write-Host -ForegroundColor Yellow 'lastexitcode=' $LASTEXITCODE;
