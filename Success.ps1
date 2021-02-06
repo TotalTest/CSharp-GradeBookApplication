f3c87667244787600941ab82c1dffc5573682b24
Prab <prab@totaltestltd.co.uk>
2021-02-06 20:15:57 +0000

    t

--- Success.ps1
+++ Success.ps1
@@ -14,6 +14,11 @@ git push -u origin testh
 
 echo $LASTEXITCODE
 
+if ($LASTEXITCODE -ne 0)  { 
+       Write-Host -ForegroundColor Yellow 'lastexitcode=' $LASTEXITCODE;
+       exit $LASTEXITCODE 
+  }
+
 #$urlvsts = "https://dev.azure.com/totaltestltd/Total%20Test/_apis/build/builds?api-version=6.1-preview.6"
 #$token = "7zoj7msfjzews5kkjrwd3gu65i4ldvqmnr3d34tfrtvpvhqo7xqq"
 #$encodedPat = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes( ":$token"))
